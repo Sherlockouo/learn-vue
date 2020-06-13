56f9f9e000404b3c92c917019a97d0fd24002a44
myqq12345@outlook.com <myqq123456@outlook.com>
2020-06-13 22:47:09 +0800

    add little

---
+++ study/12-模块化/01-模块化初体验/expt.js
@@ -0,0 +1,11 @@
+
+let flag = true
+
+function sum(s1,s2) {
+  return s1+s2
+}
+
+module.exports = {
+  flag:flag,
+  sum:sum
+}
---
+++ study/12-模块化/02-ES6的模块实现/tes.js
@@ -0,0 +1,41 @@
+let name = '小明'
+let age =12
+let flag = true
+
+function sum(s1,s2) {
+  return s1+s2
+}
+
+if(flag){
+  console.log(sum(20, 20));
+}
+
+//1.导出方式一
+export {
+  flag,sum
+}
+
+//2.导出方式二
+export let num = 1000
+export let height = 1.88
+
+// 3。导入函数
+export function mul(m1,m2) {
+  return m1*m2
+}
+
+// 4.导入类
+export class Person {
+  run(){
+    console.log('running...');
+  }
+}
+
+// 5 export default 只能有一个
+// const addr = 'chengdu'
+// export default addr
+
+export default function (arg) {
+  console.log(arg)
+}
+
